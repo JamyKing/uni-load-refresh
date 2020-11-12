990ca126d7de58eaad563dc1dd5d9a5facd534b6
Rainy <JianK6661@163.com>
2020-11-12 13:55:49 +0800

    动画修改

--- components/load-refresh/animation.vue
+++ components/load-refresh/animation.vue
@@ -2,18 +2,18 @@
 	<view class="content" :style="{'--color': color, '--playState': playState}">
 		<view v-if="type === 'hollowDots'" class="refresh hollow-dots-spinner">
 			<view class="dot"></view>
-			<view class="dot play-state"></view>
-			<view class="dot play-state"></view>
+			<view class="dot"></view>
+			<view class="dot"></view>
 		</view>
-		<view v-if="type === 'circlesRhombuses'" class="refresh circles-to-rhombuses-spinner">
-			<view class="circle"></view>
-			<view class="circle"></view>
-			<view class="circle"></view>
+		<view v-if="type === 'halfCircle'" class="refresh half-circle-spinner">
+			<view class="circle circle-1"></view>
+			<view class="circle circle-2"></view>
 		</view>
-		<view v-if="type === 'loopingRhombuses'" class="refresh looping-rhombuses-spinner">
-			<view class="rhombus"></view>
-			<view class="rhombus"></view>
-			<view class="rhombus"></view>
+		<view v-if="type === 'swappingSquares'" class="refresh swapping-squares-spinner">
+			<view class="square"></view>
+			<view class="square"></view>
+			<view class="square"></view>
+			<view class="square"></view>
 		</view>
 	</view>
 </template>
@@ -67,10 +67,10 @@
 	
 	/* HollowDots  */
 	.hollow-dots-spinner .dot {
-	  width: 15px;
-	  height: 15px;
-	  margin: 0 calc(15px / 2);
-	  border: calc(15px / 5) solid $color;
+	  width: 30rpx;
+	  height: 30rpx;
+	  margin: 0 calc(30rpx / 2);
+	  border: calc(30rpx / 5) solid $color;
 	  border-radius: 50%;
 	  float: left;
 	  transform: scale(0);
@@ -95,84 +95,81 @@
 	  }
 	}
 	
-	/* circlesRhombuses  */
-	.circles-to-rhombuses-spinner .circle {
-	  height: 15px;
-	  width: 15px;
-	  margin-left: calc(15px * 1.125);
-	  transform: rotate(45deg);
-	  border-radius: 10%;
-	  border: 3px solid $color;
-	  overflow: hidden;
-	  background: transparent;
-	  animation: circlesRhombuses 1200ms linear infinite;
+	/* halfCircle  */
+	.half-circle-spinner .circle {
+	  content: "";
+	  position: absolute;
+	  width: 60rpx;
+	  height: 60rpx;
+	  border-radius: 100%;
+	  border: calc(60rpx / 10) solid transparent;
 	}
-	.circles-to-rhombuses-spinner .circle:nth-child(1) {
-	  animation-delay: calc(150ms * 1);
-	  margin-left: 0
+
+	.half-circle-spinner .circle.circle-1 {
+	  border-top-color: $color;
+	  animation: halfCircle 1s infinite;
 	}
-	.circles-to-rhombuses-spinner .circle:nth-child(2) {
-	  animation-delay: calc(150ms * 2);
+	.half-circle-spinner .circle.circle-2 {
+	  border-bottom-color: $color;
+	  animation: halfCircle 1s infinite alternate;
 	}
-	.circles-to-rhombuses-spinner .circle:nth-child(3) {
-	  animation-delay: calc(150ms * 3);
-	}
-	@keyframes circlesRhombuses {
+	@keyframes halfCircle {
 	  0% {
-		border-radius: 10%;
+		transform: rotate(0deg);
 	  }
-	  17.5% {
-		border-radius: 10%;
-	  }
-	  50% {
-		border-radius: 100%;
-	  }
-	  93.5% {
-		border-radius: 10%;
-	  }
-	  100% {
-		border-radius: 10%;
-	  }
-	}
-	@keyframes circles-to-rhombuses-background-animation {
-	  50% {
-		opacity: 0.4;
+	  100%{
+		transform: rotate(360deg);
 	  }
 	}
 	
-	/* loopingRhombuses */
-	.looping-rhombuses-spinner {
-		position: relative;
+	/* swappingSquares */
+	.swapping-squares-spinner {
+	  position: relative;
 	}
-	.looping-rhombuses-spinner .rhombus {
-	  height: 15px;
-	  width: 15px;
-	  background-color: $color;
-	  left: 58%;
+	.swapping-squares-spinner .square {
+	  height: calc(65rpx * 0.25 / 1.3);
+	  width:  calc(65rpx * 0.25 / 1.3);
+	  animation-duration: 1000ms;
+	  border: calc(65rpx * 0.04 / 1.3) solid $color;
+	  margin-right: auto;
+	  margin-left: auto;
 	  position: absolute;
-	  margin: 0 auto;
-	  border-radius: 2px;
-	  transform: translateY(0) rotate(45deg) scale(0);
-	  animation: loopingRhombuses 2500ms linear infinite;
+	  animation-iteration-count: infinite;
 	}
-	.looping-rhombuses-spinner .rhombus:nth-child(1) {
-	  animation-delay: calc(2500ms * 1 / -1.5);
+	.swapping-squares-spinner .square:nth-child(1) {
+	  animation-name: swappingSquares-child-1;
+	  animation-delay: 500ms;
 	}
-	.looping-rhombuses-spinner .rhombus:nth-child(2) {
-	  animation-delay: calc(2500ms * 2 / -1.5);
+	.swapping-squares-spinner .square:nth-child(2) {
+	  animation-name: swappingSquares-child-2;
+	  animation-delay: 0ms;
 	}
-	.looping-rhombuses-spinner .rhombus:nth-child(3) {
-	  animation-delay: calc(2500ms * 3 / -1.5);
+	.swapping-squares-spinner .square:nth-child(3) {
+	  animation-name: swappingSquares-child-3;
+	  animation-delay: 500ms;
 	}
-	@keyframes loopingRhombuses {
-	  0% {
-		transform: translateX(0) rotate(45deg) scale(0);
-	  }
+	.swapping-squares-spinner .square:nth-child(4) {
+	  animation-name: swappingSquares-child-4;
+	  animation-delay: 0ms;
+	}
+	@keyframes swappingSquares-child-1 {
 	  50% {
-		transform: translateX(-233%) rotate(45deg) scale(1);
+		transform: translate(150%,150%) scale(2,2);
 	  }
-	  100% {
-		transform: translateX(-466%) rotate(45deg) scale(0);
+	}
+	@keyframes swappingSquares-child-2 {
+	  50% {
+		transform: translate(-150%,150%) scale(2,2);
+	  }
+	}
+	@keyframes swappingSquares-child-3 {
+	  50% {
+		transform: translate(-150%,-150%) scale(2,2);
+	  }
+	}
+	@keyframes swappingSquares-child-4 {
+	  50% {
+		transform: translate(150%,-150%) scale(2,2);
 	  }
 	}
 </style>
